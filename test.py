8be53140f95bbb4175fb8f6b031a562c13f2db91
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-18 23:10:00 +1100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import tensorflow_hub as hub
 import streamlit as st
-import urllib
+import urllib.request
 
 st.write("hello world")
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 #embeddings = embed(["Hello World", "Cats and dogs"])
 
 #st.write(embeddings)
-urllib.urlretrieve("https://www.dropbox.com/s/12t9fbnp6skcjun/embeddings.pickle?dl=1", "embeddings.pickle")
+urllib.request.urlretrieve("https://www.dropbox.com/s/12t9fbnp6skcjun/embeddings.pickle?dl=1", "embeddings.pickle")
     
 with open('embeddings.pickle', 'rb') as fp:
     embeddings = pickle.load(fp)
