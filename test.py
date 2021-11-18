a8d341cc4e4fc2b0fb3101c281e1ce0d3cf0f954
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-18 23:03:59 +1100

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 #embeddings = embed(["Hello World", "Cats and dogs"])
 
 #st.write(embeddings)
+!curl https://www.dropbox.com/s/12t9fbnp6skcjun/embeddings.pickle?dl=1 > embeddings.pickle
+    
 with open('embeddings.pickle', 'rb') as fp:
     embeddings = pickle.load(fp)
 with open('data.pickle','rb') as fp:
