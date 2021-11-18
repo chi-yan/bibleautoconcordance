dd74fbd2457687dbcf78eafeda4f3e100ef048fe
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-18 23:01:21 +1100

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,12 @@ import streamlit as st
 
 st.write("hello world")
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-embeddings = embed(["Hello World", "Cats and dogs"])
+#embeddings = embed(["Hello World", "Cats and dogs"])
 
-st.write(embeddings)
+#st.write(embeddings)
+with open('embeddings.pickle', 'rb') as fp:
+    embeddings = pickle.load(fp)
+with open('data.pickle','rb') as fp:
+    d = pickle.load(fp)
+with open('versedict (1).pickle','rb') as fp:
+    versedict = pickle.load(fp)
