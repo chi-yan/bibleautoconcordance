a5e5cf924ffe7dd8740ed9cf34b1df1b8bdc5aab
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-18 23:17:18 +1100

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,24 @@ import tensorflow_hub as hub
 import streamlit as st
 import urllib.request
 import pickle
+import numpy as np
+from collections import OrderedDict
+from operator import itemgetter
+import pandas as pd
+import string
+
+def cosine_similarities(A, B): #A is the big matrix containing all embeddings, B is the sentence
+    return np.divide(np.dot(A,B),np.apply_along_axis(np.linalg.norm, 1, A)*np.linalg.norm(B))[:,0]
+    
+def generateSimilarities(text, numberOfItems):
+  embedded_text = embed([text])
+  similarities = cosine_similarities(embeddings.numpy(), embedded_text.numpy().T)
+  data = []
+  similarities[0] = -0.1
+  for i in range(len(embeddings)):
+    data.append([versedict[i],d[versedict[i]],similarities[i]])
+  return(pd.DataFrame (data, columns = ['Verse', 'Text', 'Similarity']).sort_values(by=['Similarity'], ascending=Fa
+lse).head(numberOfItems))
 
 st.write("hello world")
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
