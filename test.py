c3df99bd1fa89138ea904aeb4be7a40e5c4a9b28
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-18 22:59:34 +1100

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ import tensorflow_hub as hub
 import streamlit as st
 
 st.write("hello world")
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+embeddings = embed(["Hello World", "Cats and dogs"])
+
+st.write(embeddings)
