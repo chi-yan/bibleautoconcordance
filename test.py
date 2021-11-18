b01639c19e1c94d831b6bebe46a651b93ad8b10b
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-18 23:51:46 +1100

    Update test.py

--- test.py
+++ test.py
@@ -24,11 +24,11 @@ def generateSimilarities(text, numberOfItems):
   st.write('debug 3')
   return(pd.DataFrame (data, columns = ['Verse', 'Text', 'Similarity']).sort_values(by=['Similarity'], ascending=False).head(numberOfItems))
 
-st.write("hello world")
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-#embeddings = embed(["Hello World", "Cats and dogs"])
 
-#st.write(embeddings)
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+
+
 urllib.request.urlretrieve("https://www.dropbox.com/s/12t9fbnp6skcjun/embeddings.pickle?dl=1", "embeddings.pickle") #too big to upload to Github
     
 with open('embeddings.pickle', 'rb') as fp:
@@ -38,6 +38,12 @@ with open('data.pickle','rb') as fp:
 with open('versedict (1).pickle','rb') as fp:
     versedict = pickle.load(fp)
     
-sentence="world created"
-st.write(str(list(generateSimilarities(sentence,5).to_records(index=False))))
+st.markdown("""<h1>Bible Auto-Concordance Web App</h1> <p>Enter a sentence, phrase or word you want to look for in the KJV Bible.</p><p>Built with NLP, Google Cloud, Flask, Streamlit, AgGrid and the Universal Sentence Encoder.</p><p>Works best on a desktop, if you are trying this from a mobile device, try ending the sentence with a space or a dot.</p>""", unsafe_allow_html=True)
+sentence = st.text_input('Input sentence here: ')
+
+if sentence:
+    response = str(list(generateSimilarities(sentence,5).to_records(index=False)))
+    st.write(response)
+    
+#st.write(str(list(generateSimilarities(sentence,5).to_records(index=False))))
 st.write("ok")
